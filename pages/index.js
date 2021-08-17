767f59bbe4f3953193e3762b0fbed012cfd406c5
tomoyukikashiro <ksrtmyk@gmail.com>
2021-08-17 11:21:21 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 const Index  = () => {
-  return (<h1>preview test page!!!! !!!!!</h1>)
+  return (
+    <>
+      <h1>preview test page!!!! !!!!!</h1>
+      <p>VERCEL_ENV: {process.env.VERCEL_ENV}</p>
+      <p>VERCEL_GIT_COMMIT_REF: {process.env.VERCEL_GIT_COMMIT_REF}</p>
+      <p>NEXT_PUBLIC_HOGE: {process.env.NEXT_PUBLIC_HOGE}</p>
+    </>
+  )
 }
 
 export default Index
