c44c1ebdf1c1f1017f7f037ce985276b58f7e1a6
tomoyukikashiro <ksrtmyk@gmail.com>
2021-08-17 11:28:39 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ const Index  = () => {
     <>
       <h1>preview test page!!!! !!!!!</h1>
       <p>VERCEL_ENV: {process.env.VERCEL_ENV}</p>
+      <p>NEXT_PUBLIC_VERCEL_ENV: {process.env.NEXT_PUBLIC_VERCEL_ENV}</p>
       <p>VERCEL_GIT_COMMIT_REF: {process.env.VERCEL_GIT_COMMIT_REF}</p>
+      <p>NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF: {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}</p>
       <p>NEXT_PUBLIC_HOGE: {process.env.NEXT_PUBLIC_HOGE}</p>
     </>
   )
